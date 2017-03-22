1d44734fa1e1e20315ff829049ca62d6dc4a6124
Sebastian <dr.sebastian.koehler@gmail.com>
2017-03-22 17:26:58 +0100

    first pass at a running version for @visze

--- src/main/java/drseb/AnnotationSimulator.java
+++ src/main/java/drseb/AnnotationSimulator.java
@@ -1,11 +1,16 @@
 package drseb;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import hpo.DiseaseEntry;
 import hpo.DiseaseId;
+import hpo.DiseaseId.DiseaseDatabase;
 import hpo.HpoDataProvider;
+import hpo.QueryModification;
 import ontologizer.go.Ontology;
+import ontologizer.go.Term;
+import util.HpoHelper;
 
 /**
  * Main entry point.
@@ -17,9 +22,11 @@ public class AnnotationSimulator {
 
 	private Ontology ontology;
 	private HashMap<DiseaseId, DiseaseEntry> annotations;
+	private HpoHelper hpoHelper;
+	private QueryModification queryModification;
 
 	public AnnotationSimulator(String oboOntology, String annotationFile, String separator, int objectIdColumn, int termColumn) {
-		System.err.println("not implemented so far...");
+		throw new RuntimeException("not implemented so far...");
 	}
 
 	public AnnotationSimulator(String oboOntology, String annotationFile, OntologyProjectType type) {
@@ -32,19 +39,60 @@ public class AnnotationSimulator {
 			HpoDataProvider hpoapi = new HpoDataProvider();
 			hpoapi.setOboFile(oboOntology);
 			hpoapi.setAnnotationFile(annotationFile);
-			hpoapi.parseOntoAndAssociations();
+			hpoapi.parseOntologyAndAssociations();
 
-			this.ontology = hpoapi.getHpo();
+			this.hpoHelper = hpoapi.getHpoHelper();
 			this.annotations = hpoapi.getDiseaseId2entry();
+			if (hpoHelper == null || hpoHelper.getHpo() == null) {
+				throw new RuntimeException("hpo-helper or hpo null. invalid state");
+			}
+			if (annotations == null || annotations.keySet().size() < 1) {
+				throw new RuntimeException("annotations have not been parse properly. invalid state");
+			}
+
 		}
 		else {
-			System.err.println("not implemented so far...");
+			throw new RuntimeException("not implemented so far...");
 		}
+
+		queryModification = new QueryModification(hpoHelper.getOrganAbnormalitySubgraph(), hpoHelper.getOrganAbnormalitySubgraphSlim());
 
 	}
 
-	public String getHelloWorld() {
-		return "Hello World";
+	public int getNumberObjectsAnnotated() {
+		return annotations.keySet().size();
+	}
+
+	public int getNumberOfAnnotations() {
+		return annotations.values().stream().mapToInt(e -> e.getAnnotations().size()).sum();
+	}
+
+	public ArrayList<ArrayList<Term>> simulatePatients(DiseaseDatabase diseaseDb, String diseaseIdent, int numberOfPatients,
+			double fractionOfNoiseTerms, double chanceOfBeingMappedUp, int querySize) {
+		return simulatePatients(diseaseDb, diseaseIdent, numberOfPatients, fractionOfNoiseTerms, chanceOfBeingMappedUp, querySize, querySize);
+	}
+
+	public ArrayList<ArrayList<Term>> simulatePatients(DiseaseDatabase diseaseDb, String diseaseIdent, int numberOfPatients,
+			double fractionOfNoiseTerms, double chanceOfBeingMappedUp, int lowerBoundQuerySize, int upperBoundQuerySize) {
+
+		DiseaseId id = new DiseaseId(diseaseDb, diseaseIdent);
+		if (!annotations.containsKey(id)) {
+			throw new IllegalArgumentException("No disease with annotations found for " + id);
+		}
+
+		if (numberOfPatients < 1) {
+			throw new IllegalArgumentException("Number of patients must be larger than 0.");
+		}
+
+		DiseaseEntry diseaseEntry = annotations.get(id);
+		ArrayList<ArrayList<Term>> simulatedQueries = new ArrayList<>();
+		for (int i = 0; i < numberOfPatients; i++) {
+			ArrayList<Term> patientAnnotations = queryModification.generateAnnotationSet(diseaseEntry.getAnnotations(), fractionOfNoiseTerms,
+					chanceOfBeingMappedUp, lowerBoundQuerySize, upperBoundQuerySize);
+			simulatedQueries.add(patientAnnotations);
+		}
+
+		return simulatedQueries;
 	}
 
 }
--- src/test/java/drseb/TestSimulator.java
+++ src/test/java/drseb/TestSimulator.java
@@ -10,17 +10,20 @@ public class TestSimulator {
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
+		System.out.println("setUpBeforeClass");
 		AnnotationSimulator simulator = new AnnotationSimulator("src/test/resources/hp.obo", "src/test/resources/phenotype_annotation.tab",
 				OntologyProjectType.HPO);
 		TestSimulator.simulator = simulator;
-		System.out.println("initialised: " + simulator);
 	}
 
 	@Test
-	public void test() {
-		Assert.assertEquals(simulator.getHelloWorld(), "Hello World");
+	public void testNumberDiseases() {
+		Assert.assertTrue("Number of disease must be higher than 9000", simulator.getNumberObjectsAnnotated() > 9000);
+	}
 
-		// fail("Not yet implemented");
+	@Test
+	public void testNumberAnnotations() {
+		Assert.assertTrue("Number of annotation must be higher than 120,000", simulator.getNumberOfAnnotations() > 120000);
 	}
 
 }
